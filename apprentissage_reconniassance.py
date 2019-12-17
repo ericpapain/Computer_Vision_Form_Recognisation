c4ead72394a5d67a3376d616c40a30b7851076be
Eric Papain <36469392+ericpapain@users.noreply.github.com>
2019-12-17 15:16:53 +0700

    Add files via upload

--- apprentissage_reconniassance.py
+++ apprentissage_reconniassance.py
@@ -11,7 +11,7 @@ import os
 import numpy as np
 import pickle
 
-image_dir='data/fichier_image/photos_foot'
+image_dir='data/photos'
 
 current_id=0
 label_ids={}
@@ -19,8 +19,13 @@ x_train=[]
 y_labels=[]
 
 for root, dirs, files in os.walk(image_dir):
+    """ on verifie s'il ya des image"""
     if len(files):
+        
+        """recuperation du nom du repertoire qui correspond au nom de l'acteur"""
+        
         label=root.split("/")[-1]
+        """parcours de tous le tableau files et recuperation des png"""
         for file in files:
             if file.endswith("png"):
                 path=os.path.join(root, file)
@@ -28,15 +33,27 @@ for root, dirs, files in os.walk(image_dir):
                     label_ids[label]=current_id
                     current_id+=1
                 id_=label_ids[label]
-                image=cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-                x_train.append(image)
-                y_labels.append(id_)
+                
+                #redimensionnement de l'image pour permettre a notre modèle d'être plus compétent
+                image=cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE), (150, 150))
+              
+                #verification de la netteter dans les image et fixage du seuil de selection des images a enregistrer dans la dataset
+                #le laplacien permet de voir si une photos est floue ou non
+                fm=cv2.Laplacian(image, cv2.CV_64F).var()
+                if fm<150:
+                    print("Photo exclue:", path, fm)
+                else:
+                    x_train.append(image)
+                    y_labels.append(id_)
+                    
 
 with open("labels.pickle", "wb") as f:
     pickle.dump(label_ids, f)
 
 x_train=np.array(x_train)
 y_labels=np.array(y_labels)
+
+#fonction de reconnaissance d'image
 recognizer=cv2.face.LBPHFaceRecognizer_create()
 recognizer.train(x_train, y_labels)
-recognizer.save("trainner.yml")
+recognizer.save("trainner.yml") 
---
+++ train_cnn_opencv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 03:04:50 2019
+
+@author: eric
+"""
+
